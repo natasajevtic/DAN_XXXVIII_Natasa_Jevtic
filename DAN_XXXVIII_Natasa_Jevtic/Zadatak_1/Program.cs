423c13fe862e597a381f7788a297a5bbead43a04
natasajevtic <nataly.jevtic.95@gmail.com>
2020-06-24 22:15:41 +0200

    Created class Truck

--- DAN_XXXVIII_Natasa_Jevtic/Zadatak_1/Program.cs
+++ DAN_XXXVIII_Natasa_Jevtic/Zadatak_1/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
+    /// <summary>
+    /// This program simulates charge and discharge of trucks using threads.
+    /// </summary>
     class Program
     {
+        static Truck[] trucks = new Truck[10];        
+
         static void Main(string[] args)
         {
+            //creating 10 objects of class Truck and setting their names
+            for (int i = 0; i < trucks.Length; i++)
+            {
+                trucks[i] = new Truck() { Name = string.Format("Truck_{0}", i + 1) };
+            }
         }
     }
 }
---
+++ DAN_XXXVIII_Natasa_Jevtic/Zadatak_1/Truck.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Zadatak_1
+{
+    class Truck
+    {
+        public int Route { get; set; }
+        public int TimeOfDelivery { get; set; }
+        public int TimeOfCharge { get; set; }
+        public int TimeOfDischarge { get; set; }
+        public string Name { get; set; }
+
+        public void Charge() { }       
+
+        public void Delivery() { }
+        
+        public void Discharge() { }
+    }
+}
